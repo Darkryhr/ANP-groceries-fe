2b4dc620a792584e07d4d4bdbc46fbbde4d3745e
gabriel <gabyk800@gmail.com>
2023-09-21 16:46:35 +0300

    custom theme

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { LayoutComponent } from './layout/layout.component';
 
-const components = [];
+const components = [LayoutComponent];
 
-const modules = [CommonModule];
+const modules = [CommonModule, MatToolbarModule];
 
 @NgModule({
-  declarations: [...components],
+  declarations: [...components, LayoutComponent],
   imports: [...modules],
   exports: [...modules, ...components],
 })
