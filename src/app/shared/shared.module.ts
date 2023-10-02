d37a939108a0c2228434d0193556bd5282c31496
gabriel <gabyk800@gmail.com>
2023-10-02 17:09:40 +0300

    basic top nav

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { LayoutComponent } from './layout/layout.component';
 
 const components = [LayoutComponent];
 
-const modules = [CommonModule, MatToolbarModule];
+const modules = [
+  CommonModule,
+  MatToolbarModule,
+  MatButtonModule,
+  FormsModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+];
 
 @NgModule({
   declarations: [...components, LayoutComponent],
